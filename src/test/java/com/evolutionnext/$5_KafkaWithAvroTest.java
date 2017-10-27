f0cac4069fe15afc0810f1b3a42df052144cacc3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-10-27 13:09:17 -0400

    Last Examples

--- src/test/java/com/evolutionnext/$5_KafkaWithAvroTest.java
+++ src/test/java/com/evolutionnext/$5_KafkaWithAvroTest.java
@@ -1,39 +1,66 @@
 package com.evolutionnext;
 
+import com.evolutionnext.avro.Album;
+import com.evolutionnext.avro.Artist;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class $5_KafkaWithAvroTest {
+
+
     @Test
-    public void testSend() throws ExecutionException, InterruptedException {
-            Properties properties = new Properties();
-            properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                    "kaf0:9092,kaf1:9092,kaf2:9092"); //At least 2
-            properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                    "org.apache.kafka.common.serialization.StringSerializer");
-            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                    "io.confluent.kafka.serializers.KafkaAvroSerializer");
-            properties.put("schema.registry.url", "");
-            KafkaProducer<String, String> producer =
-                    new KafkaProducer<>(properties);
-            ProducerRecord<String, String> producerRecord =
-                    new ProducerRecord<>("scaled-objects",
-                            "");
-            Future<RecordMetadata> future = producer.send(producerRecord);
-            producer.flush();
-            RecordMetadata recordMetadata = future.get();  //blocks
-            System.out.format("offset: %d\n", recordMetadata.offset());
-            System.out.format("partition: %d\n", recordMetadata.partition());
-            System.out.format("timestamp: %d\n", recordMetadata.timestamp());
-            System.out.format("topic: %s\n", recordMetadata.topic());
-            System.out.format("toString: %s\n", recordMetadata.toString());
-        }
+    public void testSend() throws ExecutionException, InterruptedException, IOException {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "kaf0:9092,kaf1:9092,kaf2:9092"); //At least 2
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        String userSchema = "{\"type\":\"record\"," +
+                "\"name\":\"myrecord\"," +
+                "\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}";
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse(userSchema);
+        GenericRecord avroRecord = new GenericData.Record(schema);
+
+        avroRecord.put("f1", "value1");
+
+        SpecificDatumWriter<GenericRecord> writer = new SpecificDatumWriter<>(schema);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        writer.write(avroRecord, encoder);
+        encoder.flush();
+
+        KafkaProducer<String, Object> producer =
+                new KafkaProducer<>(properties);
+        ProducerRecord<String, Object> producerRecord =
+                new ProducerRecord<>("scaled-avro", out.toByteArray());
+        Future<RecordMetadata> future = producer.send(producerRecord);
+        producer.flush();
+        RecordMetadata recordMetadata = future.get();  //blocks
+        System.out.format("offset: %d\n", recordMetadata.offset());
+        System.out.format("partition: %d\n", recordMetadata.partition());
+        System.out.format("timestamp: %d\n", recordMetadata.timestamp());
+        System.out.format("topic: %s\n", recordMetadata.topic());
+        System.out.format("toString: %s\n", recordMetadata.toString());
     }
 }
+
--- src/test/java/com/evolutionnext/$6_SparkWithKryoTest.java
+++ src/test/java/com/evolutionnext/$6_SparkWithKryoTest.java
@@ -1,10 +1,70 @@
 package com.evolutionnext;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.serializer.KryoRegistrator;
 import org.junit.Test;
 
-public class $6_SparkWithKryoTest {
-    @Test
-    public void testProcessData() {
+import java.util.Arrays;
 
+
+//Source: https://github.com/holdenk/learning-spark-examples/blob/master/src/main/java/com/oreilly/learningsparkexamples/java/BasicAvgWithKryo.java
+public class $6_SparkWithKryoTest {
+
+    static class AvgCount implements java.io.Serializable {
+        public AvgCount() {
+            total_ = 0;
+            num_ = 0;
+        }
+
+        public AvgCount(int total, int num) {
+            total_ = total;
+            num_ = num;
+        }
+
+        public float avg() {
+            return total_ / (float) num_;
+        }
+
+        public int total_;
+        public int num_;
+    }
+
+    public static class AvgRegistrator implements KryoRegistrator {
+        public void registerClasses(Kryo kryo) {
+            kryo.register(AvgCount.class, new FieldSerializer(kryo, AvgCount.class));
+        }
+    }
+
+    @Test
+    public void testProcessData() throws InterruptedException {
+        String master = "local[*]";
+
+        SparkConf conf = new SparkConf().setMaster(master).setAppName("basicavgwithkyro");
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        conf.set("spark.kryo.registrator", AvgRegistrator.class.getName());
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4));
+        Function2<AvgCount, Integer, AvgCount> addAndCount = (Function2<AvgCount, Integer, AvgCount>) (a, x) -> {
+            a.total_ += x;
+            a.num_ += 1;
+            return a;
+        };
+        Function2<AvgCount, AvgCount, AvgCount> combine = (Function2<AvgCount, AvgCount, AvgCount>) (a, b) -> {
+            a.total_ += b.total_;
+            a.num_ += b.num_;
+            return a;
+        };
+        AvgCount initial = new AvgCount(0, 0);
+        AvgCount result = rdd.aggregate(initial, addAndCount, combine);
+        System.out.println(result.avg());
+
+
+        Thread.sleep(3 * 60 * 1000);
     }
 }
+
