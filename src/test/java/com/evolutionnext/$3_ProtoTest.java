7b9217885963ae24086cdc99f3cc36866c63ae0e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-10-27 11:51:28 -0400

    updated proto test

--- src/test/java/com/evolutionnext/$3_ProtoTest.java
+++ src/test/java/com/evolutionnext/$3_ProtoTest.java
@@ -1,30 +1,32 @@
 package com.evolutionnext;
 
-import com.evolutionnext.proto.AddressBookProtos;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import com.evolutionnext.proto.AddressBookProtos;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class $3_ProtoTest {
+    //AddressBookProtos.Person.PhoneType.HOME
     @Test
     public void testSerializeAndDeserialize() throws IOException, InterruptedException {
         AddressBookProtos.Person.PhoneNumber address =
-                AddressBookProtos.Person.PhoneNumber.newBuilder()
-                                                    .setNumber("+351 999 999 999")
-                                                    .setType(AddressBookProtos.Person.PhoneType.HOME)
-                                                    .build();
+                com.evolutionnext.proto.AddressBookProtos.Person.PhoneNumber.newBuilder()
+                                                                            .setNumber("+351 999 999 999")
+                                                                            .setType(AddressBookProtos.Person.PhoneType.HOME)
+                                                                            .build();
 
-        AddressBookProtos.Person pre =
-                AddressBookProtos.Person.newBuilder()
-                                        .setId(12345)
-                                        .setName("Wild Belle")
-                                        .setEmail("wb@aol.com")
-                                        .addPhones(address)
-                                        .build();
+        com.evolutionnext.proto.AddressBookProtos.Person pre =
+                com.evolutionnext.proto.AddressBookProtos.Person.newBuilder()
+                                                                .setId(12345)
+                                                                .setName("Wild Belle")
+                                                                .setEmail("wb@aol.com")
+                                                                .addPhones(address)
+                                                                .build();
 
 
         FileOutputStream aOutput = new FileOutputStream("target/person.probin");
--- src/test/java/com/evolutionnext/$5_KafkaWithAvroTest.java
+++ src/test/java/com/evolutionnext/$5_KafkaWithAvroTest.java
@@ -1,9 +1,39 @@
 package com.evolutionnext;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.Test;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class $5_KafkaWithAvroTest {
     @Test
-    public void testSend() {
+    public void testSend() throws ExecutionException, InterruptedException {
+            Properties properties = new Properties();
+            properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                    "kaf0:9092,kaf1:9092,kaf2:9092"); //At least 2
+            properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                    "org.apache.kafka.common.serialization.StringSerializer");
+            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                    "io.confluent.kafka.serializers.KafkaAvroSerializer");
+            properties.put("schema.registry.url", "");
+            KafkaProducer<String, String> producer =
+                    new KafkaProducer<>(properties);
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<>("scaled-objects",
+                            "");
+            Future<RecordMetadata> future = producer.send(producerRecord);
+            producer.flush();
+            RecordMetadata recordMetadata = future.get();  //blocks
+            System.out.format("offset: %d\n", recordMetadata.offset());
+            System.out.format("partition: %d\n", recordMetadata.partition());
+            System.out.format("timestamp: %d\n", recordMetadata.timestamp());
+            System.out.format("topic: %s\n", recordMetadata.topic());
+            System.out.format("toString: %s\n", recordMetadata.toString());
+        }
     }
 }
